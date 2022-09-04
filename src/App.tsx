7d2fb5a4b7d9f33c1fbbb825ff449c3eb2e4fc77
Carlos D. Di Matteo <carlosdima97@gmail.com>
2022-09-03 19:07:02 -0500

    moved files around

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,21 @@
 import { GlobalStyle } from './theme/GlobalStyles';
 import { ThemeProvider } from 'styled-components';
-import { Wrapper } from './Wrapper';
+import { Wrapper } from './components/layout/Wrapper';
 import { AppTheme } from './theme';
-import { Topbar } from './components/Topbar/Topbar';
-import { Sidebar } from './components/Sidebar/Sidebar';
+import { Topbar } from './components/layout/Topbar/Topbar';
+import { Sidebar } from './components/layout/Sidebar/Sidebar';
 
 function App() {
-  return (
-    <ThemeProvider theme={AppTheme}>
-      <GlobalStyle />
-        <Topbar />
-        <Wrapper>
-          <Sidebar />
-          <div />
-        </Wrapper>
-    </ThemeProvider>
-  );
+	return (
+		<ThemeProvider theme={AppTheme}>
+			<GlobalStyle />
+			<Topbar />
+			<Wrapper>
+				<Sidebar />
+				<div />
+			</Wrapper>
+		</ThemeProvider>
+	);
 }
 
 export default App;
