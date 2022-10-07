2749f7a5d30f7493f13a5fa1cae7d6bb4e828d34
Carlos D. Di Matteo <carlosdima97@gmail.com>
2022-10-07 18:45:08 -0500

    add chain notification socket hook implementation

---
+++ src/hooks/useChainNotifications.ts
@@ -0,0 +1,50 @@
+import { useEffect, useReducer } from 'react';
+
+const url = process.env.REACT_APP_FULL_NODE_WS_URL;
+function notificationReducer(
+	state: any[],
+	{ type, payload }: { type: string; payload?: any },
+) {
+	switch (type) {
+		case 'reset':
+			return payload || [];
+
+		case 'append':
+			const newNotifications = payload;
+			return [...newNotifications, ...state];
+	}
+}
+let socketClient: WebSocket = null;
+export function useChainNotifications() {
+	const [notifications, setNotifications] = useReducer(notificationReducer, []);
+
+	useEffect(() => {
+		if (!socketClient) {
+			socketClient = new WebSocket(url);
+			// Connection opened
+			socketClient.addEventListener('open', (event) => {
+				socketClient.send(
+					'{ "jsonrpc": "2.0", "id":1, "method": "Filecoin.ChainNotify", "params": [ ] }',
+				);
+			});
+
+			// Listen for messages
+			socketClient.addEventListener('message', (event) => {
+				const data = JSON.parse(event.data);
+				if (notifications.length < 20) {
+					setNotifications({
+						type: 'append',
+						payload: data.params[1][0].Val.Blocks,
+					});
+				} else {
+					setNotifications({
+						type: 'reset',
+						payload: data.params[1][0].Val.Blocks,
+					});
+				}
+			});
+		}
+	}, []);
+
+	return notifications;
+}
