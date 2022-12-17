3f4c02d4a3b6caed04662e05e9b34245479e1b2f
Cory Schwartz <cory@protocol.ai>
2022-12-17 01:31:03 -0800

    use tmp-gateway.chain.love

--- src/api/chain.love.ts
+++ src/api/chain.love.ts
@@ -16,7 +16,7 @@ export class ChainLoveAPI {
 		const { data } = await axios.post(
 			API_TYPE === 'fullnode'
 				? `${process.env.REACT_APP_FULL_NODE_API_URL}`
-				: `http://ac16c63841bd149668047ae16ca77fe5-170719103.us-east-1.elb.amazonaws.com`,
+				: `tmp-gateway.chain.love`,
 				// : `${process.env.REACT_APP_GATEWAY_API_URL}`,
 			this.getJSONRPCBody(method, params),
 			{
